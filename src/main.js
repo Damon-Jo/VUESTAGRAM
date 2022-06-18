57db092b5fa58411287995cdcbc4aaa0b9de2c3b
Damon-Jo <lunaticjky@gmail.com>
2022-06-18 15:55:17 -0700

    create store.js and install vuex

--- src/main.js
+++ src/main.js
@@ -5,4 +5,7 @@ let emitter = mitt();
 let app = createApp(App);
 app.config.globalProperties.emitter = emitter;
 //app.config.globalProperties.axios = axios;
-app.mount('#app')
+
+import store from './store.js'
+
+app.use(store).mount('#app')
